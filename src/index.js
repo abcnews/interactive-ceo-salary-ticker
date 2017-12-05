d330480fcd15b8324794f1a0f10f8bf990d267e2
Nathan Hoad <nathan@nathanhoad.net>
2017-12-05 15:20:15 +1000

    Tickers are now targeted to #salaryticker markers

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
-const { h, render } = require('preact');
+const html = require('bel');
 
-const PROJECT_NAME = 'interactive-ceo-salary-ticker';
-const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
+const styles = require('./index.scss');
+
+const START_TIME = new Date().getTime();
+const CEO_PER_SECOND = 4750326.6 / 31557600 / 10; // seconds in a year
+const WORKER_PER_SECOND = 60900 / 31557600 / 10;
+
+function all(query) {
+  return [].slice.call(document.querySelectorAll(query));
+}
 
 function init() {
-  const App = require('./components/App');
-  render(<App ceo={4750326.6} worker={60900} />, root, root.firstChild);
+  all('[name^=salaryticker').forEach(element => {
+    const ticker = html`
+      <div class="${styles.base}">
+        <p>Since you started reading this story, the average CEO has earned</p>
+        <div class="${styles.ceoSalary}">$0.00</div>
+        <p>Meanwhile, the average Australian has earned</p>
+        <div class="${styles.workerSalary}">$0.00</div>
+      </div>`;
+
+    // Insert this new element
+    element.parentNode.insertBefore(ticker, element);
+  });
+
+  // Count up
+  setInterval(() => {
+    const seconds = (new Date().getTime() - START_TIME) / 100;
+    all('.' + styles.ceoSalary).forEach(el => (el.innerText = '$' + (seconds * CEO_PER_SECOND).toFixed(2)));
+    all('.' + styles.workerSalary).forEach(el => (el.innerText = '$' + (seconds * WORKER_PER_SECOND).toFixed(2)));
+  }, 100);
 }
 
 init();
-
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    try {
-      init();
-    } catch (err) {
-      const ErrorBox = require('./components/ErrorBox');
-      render(<ErrorBox error={err} />, root, root.firstChild);
-    }
-  });
-}
-
-if (process.env.NODE_ENV === 'development') {
-  require('preact/devtools');
-  console.debug(`[${PROJECT_NAME}] public path: ${__webpack_public_path__}`);
-}
