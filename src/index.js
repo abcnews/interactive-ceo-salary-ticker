999c744433ba2c63534b0f5f0c8934422bafa5a7
Joshua Byrd <phocks@gmail.com>
2022-11-29 14:35:09 +1000

    Header init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const html = require('bel');
+const html = require("bel");
+import { whenOdysseyLoaded } from "@abcnews/env-utils";
+import { selectMounts } from "@abcnews/mount-utils";
 
-const styles = require('./index.scss');
+const styles = require("./index.scss").default;
 
 const START_TIME = new Date().getTime();
 const CEO_PER_MILLISECOND = 4750326.6 / 31557600 / 1000; // milliseconds in a year
@@ -10,15 +12,18 @@ function all(query) {
   return [].slice.call(document.querySelectorAll(query));
 }
 
-function init() {
-  all('[name^=salaryticker]').forEach(element => {
-    const ticker = html`
-      <div class="${styles.base}">
-        <p>Since you started reading this story, the average CEO has earned</p>
-        <div class="${styles.ceoSalary}">$0.00</div>
-        <p>Meanwhile, the average Australian has earned</p>
-        <div class="${styles.workerSalary}">$0.00</div>
-      </div>`;
+async function init() {
+  await whenOdysseyLoaded;
+  const mounts = selectMounts("salaryticker");
+
+  // There will be only one anyway but...
+  mounts.forEach((element) => {
+    const ticker = html` <div class="${styles.base}">
+      <p>Since you started reading this story, the average CEO has earned</p>
+      <div class="${styles.ceoSalary}">$0.00</div>
+      <p>Meanwhile, the average Australian has earned</p>
+      <div class="${styles.workerSalary}">$0.00</div>
+    </div>`;
 
     // Insert this new element
     element.parentNode.insertBefore(ticker, element);
@@ -27,9 +32,14 @@ function init() {
   // Count up
   setInterval(() => {
     const milliseconds = new Date().getTime() - START_TIME;
-    all('.' + styles.ceoSalary).forEach(el => (el.innerText = '$' + (milliseconds * CEO_PER_MILLISECOND).toFixed(2)));
-    all('.' + styles.workerSalary).forEach(
-      el => (el.innerText = '$' + (milliseconds * WORKER_PER_MILLISECOND).toFixed(2))
+    all("." + styles.ceoSalary).forEach(
+      (el) =>
+        (el.innerText = "$" + (milliseconds * CEO_PER_MILLISECOND).toFixed(2))
+    );
+    all("." + styles.workerSalary).forEach(
+      (el) =>
+        (el.innerText =
+          "$" + (milliseconds * WORKER_PER_MILLISECOND).toFixed(2))
     );
   }, 16);
 }
